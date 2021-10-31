d761fbb8ded60ab59c482f5befc1a96b89916c26
amar-eng <am24mo@gmail.com>
2021-10-30 20:28:52 -0400

    landing page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import English from "./pages/English";
-import Somali from "./pages/Somali";
+// import English from "./pages/English";
+// import Somali from "./pages/Somali";
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/english" component={English} />
-        <Route path="/somali" exact component={Somali} />
+        {/* <Route path="/english" component={English} />
+        <Route path="/somali" exact component={Somali} /> */}
       </Switch>
     </Router>
   );
