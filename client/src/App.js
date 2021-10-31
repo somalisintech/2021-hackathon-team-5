406065bb5da6a15f84f9a5e55405b106c021cb52
amar-eng <am24mo@gmail.com>
2021-10-30 22:08:12 -0400

    about and about services completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import AboutServices from "./pages/AboutServices";
 // import English from "./pages/English";
 // import Somali from "./pages/Somali";
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/about-services" component={AboutServices} />
         {/* <Route path="/english" component={English} />
         <Route path="/somali" exact component={Somali} /> */}
       </Switch>
