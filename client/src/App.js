626c8f5661cfee02dea705cfb1547485af1d2cc5
elir-mahad <mmahad280@gmail.com>
2021-10-31 07:40:42 -0400

    Remove reptitions in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import English from "./pages/English";
-import Somali from "./pages/Somali";
 import SignIn from "./pages/SignIn";
 import Login from "./pages/Login";
 import UserHome from "./pages/UserHome";
@@ -11,12 +8,9 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/english" component={English} />
-        <Route path="/somali" exact component={Somali} />
         <Route path="/signup" exact component={SignIn} />
         <Route path="/login" exact component={Login} />
-        <Route path="/user-home" exact component={UserHome} />
+        <Route path="/user-home" exact component={UserHome} />{" "}
         <Route path="/user-rewards" exact component={UserRewards} />
       </Switch>
     </Router>
